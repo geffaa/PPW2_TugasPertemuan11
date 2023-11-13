47db207082a845232b83681f7f012e700e3e3eca
geffaa <yodhimas02@gmail.com>
2023-11-13 20:40:23 +0700

    first commit

---
+++ app/Models/Buku.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Buku extends Model
+{
+    protected $table = 'buku';
+
+    protected $fillable = ['id', 'judul', 'penulis', 'harga', 'tgl_terbit', 'created_at', 'updated_at', 'filename', 'filepath'];
+
+    protected $dates = ['tgl_terbit'];
+
+    public function galleries(): HasMany
+    {
+        return $this->hasMany(Gallery::class);
+    }
+}
